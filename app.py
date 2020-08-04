755b8af57d3b7def6dfe7b4226d762651dd84fda
Chukslord1 <chukslord1@gmail>
2020-08-04 11:09:31 +0100

    updated

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
 from flask import *
 app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session
+
+import mysql.connector
+import re
 
 application=app
 
+app.secret_key = 'roKBFeMEysoxUPUzVx7cRC17eUwEpJEWvxS9K7Gn'
 
-@app.route("/")
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        db = mysql.connector.connect(user='root', database='pythonlogin')
+        cursor = db.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect(url_for('index'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    return render_template('pages-login.html', msg='')
+
+@app.route("/index/")
 def index():
     return render_template("index2.html")
 
